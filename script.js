e30818105bfa8e6255b86684f045f4cb05d98ae3
parzival43 <jaisenthil43@gmail.com>
2026-07-08 19:14:15 +0530

    Mail API

--- script.js
+++ script.js
@@ -362,7 +362,7 @@ document.addEventListener("DOMContentLoaded", () => {
       };
 
       try {
-        const response = await fetch("http://127.0.0.1/api", {
+        const response = await fetch("/api", {
           method: "POST",
           headers: {
             "content-type": "application/json"
