6155beda874fac9b9863b237f5c34dcb629177ff
parzival43 <jaisenthil43@gmail.com>
2026-07-08 18:58:07 +0530

    Auto code deployment

--- updat.sh
+++ updat.sh
@@ -1,5 +1,6 @@
-filename="$(date +%Y-%m-%d)_entwinity"
+filename="$(date +%Y-%m-%d_%H:%M:%S)_entwinity"
 echo "$filename"
 mv entwinity.hopto.org $filename
+pwd
 git clone https://github.com/parzival43/Entwinity.git
 mv Entwinity entwinity.hopto.org
